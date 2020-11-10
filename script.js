79c3899e5a862bd1a468ecedffdf42969eee6f1a
SergeyRee <drrons2000@gmail.com>
2020-11-10 14:20:53 +0100

    loop

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-let num = 50;
-
-switch (num) {
-    case num < 49:
-        console.log("False!");
-        break;
-    case num < 100:
-        console.log("Too much!");
-        break;
-    case num > 80:
-        console.log("Else too much!");
-        break;
-    case 50:
-        console.log("True!");
-        break;
-    default:
-        console.log("Something wrong!");
-        break;
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        continue
+    }
+    console.log(i)
 }
+
