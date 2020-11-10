f9dc60b43d24772dadeb4b5966a6abf1cd56cf25
SergeyRee <drrons2000@gmail.com>
2020-11-10 14:04:54 +0100

    add if else

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let incr = 10,
-    decr = 10;
+let num = 50;
 
-console.log(++incr);
-console.log(--decr);
+switch (num) {
+    case num < 49:
+        console.log("False!");
+        break;
+    case num < 100:
+        console.log("Too much!");
+        break;
+    case num > 80:
+        console.log("Else too much!");
+        break;
+    case 50:
+        console.log("True!");
+        break;
+    default:
+        console.log("Something wrong!");
+        break;
+}
