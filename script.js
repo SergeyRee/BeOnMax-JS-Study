1c8cb8ba18865f4b1da685a2b0306b78f4d484a5
SergeyRee <drrons2000@gmail.com>
2020-11-13 15:21:17 +0100

    Events

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let box = document.getElementById('box'),
-    btn = document.getElementsByTagName('button'),
-    circle = document.getElementsByClassName('circle'),
-    hart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
+
+// btn[0].addEventListener('click', function(event){
+//     console.log("Произошло событие: " + event.type + ' на эллементе ' + event.target )
+// });
+
+// wrap.addEventListener('click', function() {
+//     console.log("Произошло событие: " + event.type + ' на эллементе ' + event.target )
+// });
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Произошло событие: " + event.type + ' на эллементе ' + event.target )
+});
+
+btn.forEach(function(item) {
+    item.addEventListener('mouseleave', function(){
+        console.log('Out');
+    });
+});
