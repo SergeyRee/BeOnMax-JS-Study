e72448bcf935907116835a0124549665bd478a68
SergeyRee <drrons2000@gmail.com>
2020-11-23 14:28:13 +0100

    options

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let btn = document.querySelector('button');
 
-btn.addEventListener('click', function() {
-    console.log(this);
-    this.style.backgroundColor = 'red';
-    function showThis() {
-        console.log(this);
-    }
-    showThis();
-});
+
+class Renctangle {}
